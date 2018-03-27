4b93025f35282f45ec16c5dd9f9873478d438173
ValAs1 <37310121+ValAs1@users.noreply.github.com>
2018-03-27 21:20:12 +0400

    18 19 20

--- Homework 26.03/19/Program.cs
+++ Homework 26.03/19/Program.cs
@@ -10,6 +10,19 @@ namespace _19
     {
         static void Main(string[] args)
         {
+            ////   Տպել ամենամեծ քառանիշ թիվը, որը 18-ով բազմապատկելիս դառնում է բնական թվի քառակուսի։
+
+            int b;
+            for (int i = 9999 - 1; i >= 1000; i--)
+            {
+                b = i * 18;
+                if (Math.Sqrt(b) % 1 == 0)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
--- Homework 26.03/20/Program.cs
+++ Homework 26.03/20/Program.cs
@@ -10,6 +10,20 @@ namespace _20
     {
         static void Main(string[] args)
         {
+            ////Տպել ամենափոքր եռանիշ թիվը, որի քառակուսի արմատը մեծ է տրված n բնական թվից։
+            
+            Console.Write("n - ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 100; i <= 999; i++)
+            {
+                if (Math.Sqrt(i) > n)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+            Console.ReadLine();
+
         }
     }
 }
