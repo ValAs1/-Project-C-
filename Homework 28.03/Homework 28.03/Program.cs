3f9778ec8935dfe01fec336a3bc6505e2c16474e
ValAs1 <37310121+ValAs1@users.noreply.github.com>
2018-03-29 07:30:36 +0400

    29.03

---
+++ Homework 28.03/Homework 28.03/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_28._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //erankyun  c
+            Console.Write("n  - ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = n; j > i; j--)
+                {
+
+                    ////for (int j = n; j > i; j--)
+                    ////{
+                    ////    Console.Write("#");
+                    ////}
+                    ////for (int k = n; k > n - 1; k--)
+                    ////{
+                    ////    Console.Write("*");
+                    ////}
+                 Console.Write("#");
+                }
+                for (int k = n; k > n-1; k--)
+                {
+                    Console.Write("*");
+                }
+
+
+                //for (int z = n; z > i; z++)
+                //{
+                //    Console.Write("*");
+                //}
+
+
+                Console.WriteLine();
+            }
+        
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Homework 28.03/erankyun/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace erankyun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.Write("n  - ");
+            int n = int.Parse(Console.ReadLine());
+
+            ////  erankyun  a
+
+            for (int i = 0; i <= n; i++)
+            {
+                for (int z = 0; z < i; z++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("----------------------------------------------------");
+            ////erankyun   b
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < n-i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("----------------------------------------------------");
+            ////  erankyun  c
+
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = n; j > i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("----------------------------------------------------");
+            ////erankyun  d
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = n; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = n; k > n - i; k--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
