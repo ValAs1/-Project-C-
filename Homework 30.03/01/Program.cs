2fd5e6c3d52abf080cee1e1e95dfd74a316650d7
ValAs1 <37310121+ValAs1@users.noreply.github.com>
2018-03-31 07:30:56 +0400

    30.03

---
+++ Homework 30.03/01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+      {
+        //    Console.Write("n=");
+        //    int n = int.Parse(Console.ReadLine());
+        //    int[] Array = new int[n];
+        //    for (int i = 0; i < Array.Length; i++)
+        //    {
+        //        Array[i] = Math.Sqrt(i);
+        //      Console.Write(Array[i] + ", ");
+        //    }
+        //    Console.ReadLine();
+        }
+    }
+}
---
+++ Homework 30.03/02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Հաշվե թվային զանգվածի էլեմենտների գումարը։
+
+            int[] intArray = { 10, 20, 30, 4, 55, 67, 7, 8, 9, 17 };
+            Console.Write("a=");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("b=");
+            int n1 = int.Parse(Console.ReadLine());
+            int a = 0;
+            int b = 0;
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                if (intArray[i] % n == 0)
+                {
+                    a++;
+                }
+                if (intArray[i] % n1 == 0)
+
+                {
+                    b++;
+                }
+            }
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+
+        }
+    }
+}
