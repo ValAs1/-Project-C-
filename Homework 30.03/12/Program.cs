cbe563da2725cc2f9307a1d1b670a30958601520
ValAs1 <37310121+ValAs1@users.noreply.github.com>
2018-04-03 19:34:51 +0400

    03,04

---
+++ Homework 30.03/12/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Այն  տարրերի   արտադրյալը, որոնց արժեքի  և ինդեքսի   տարբերությունը   դրական   թիվ  է։
+
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            int result = 1;
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+               if(arr[i] -i > 0)
+                {
+                    result *= arr[i];
+                    Console.WriteLine(result);
+                }
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
