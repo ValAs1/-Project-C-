6212687476b51bca7f7339a2581249913f79632e
ValAs1 <37310121+ValAs1@users.noreply.github.com>
2018-04-07 19:39:56 +0400

    06.04

---
+++ 06.04.18/01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //////// 01 Գրել ֆունկցիա, որը կվերադարձնի true, եթե արգումենտում փոխանցված ամբողջ թիվը զույգ է և false՝ եթե կենտ է։
+        }
+    }
+}
---
+++ Homework 04.04.18/05/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////// 05 Տրված են m ամբողջ թիվը և m x m չափի զանգված, որի տարրերը բնական թվեր են, հաշվել` 
+            //////       գլխավոր անկյունակծից ներքև գտնվող թվերի քանակը, որոնք բաժանվում են տրված k թվին
+        }
+    }
+}
